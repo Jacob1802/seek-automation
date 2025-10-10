f105fe11c6fdf142e5c1dd1183e78ac4a7d4abb3
Jacob1802 <Jacobrdilonardo@gmail.com>
2025-10-10 16:02:50 +1100

    build login functionality for seek client

---
+++ integrations/seek_client.py
@@ -0,0 +1,106 @@
+from integrations.mail_handler import MailClient
+from urllib.parse import urlparse, parse_qs
+from curl_cffi import requests
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
+AUTH0_CLIENT = 'eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOS4yOC4wIn0='
+CLIENTID = "yGBVge66K5NJpSN5u71fU90VcTlEASNu"
+SEEK_LOGIN_SENDER = "noreply@seek.com.au"
+
+class SeekClient:
+    def __init__(self, mail_client: MailClient):
+        self.mail_client = mail_client
+
+
+    def login(self):
+        headers = {
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.6',
+            'auth0-client': AUTH0_CLIENT,
+            'content-type': 'application/json',
+            'origin': 'https://login.seek.com',
+            'priority': 'u=1, i',
+            'referer': 'https://login.seek.com/',
+            'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Brave";v="140"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+            'x-request-language': 'en-au',
+        }
+
+        with requests.Session(impersonate="chrome", headers=headers, allow_redirects=True) as session:
+            json_data = {
+                'client_id': CLIENTID,
+                'connection': 'email',
+                'send': 'link',
+                'email': 'os.getenv("EMAIL_ADDRESS")',
+                'authParams': {
+                    'response_type': 'code',
+                    'redirect_uri': 'https://www.seek.com.au/oauth/callback/',
+                    'scope': 'openid profile email offline_access',
+                    'audience': 'https://seek/api/candidate',
+                },
+            }
+
+            response = session.post('https://login.seek.com/passwordless/start', json=json_data)
+            code = self.mail_client.fetch_code(SEEK_LOGIN_SENDER)
+            json_data = {
+                'connection': 'email',
+                'verification_code': code,
+                'email': 'os.getenv("EMAIL_ADDRESS")',
+                'client_id': CLIENTID,
+            }
+            # login
+            response = session.post('https://login.seek.com/passwordless/verify', json=json_data)
+            params = {
+                'client_id': CLIENTID,
+                'response_type': 'code',
+                'redirect_uri': 'https://www.seek.com.au/oauth/callback/',
+                'scope': 'openid profile email offline_access',
+                'audience': 'https://seek/api/candidate',
+                '_intstate': 'deprecated',
+                'protocol': 'oauth2',
+                'connection': 'email',
+                'verification_code': code,
+                'email': 'os.getenv("EMAIL_ADDRESS")',
+                'auth0Client': AUTH0_CLIENT,
+            }
+            
+            response = session.get('https://login.seek.com/passwordless/verify_redirect', params=params, headers=headers)
+            auth_code = self._parse_auth_code(response.url)
+
+            if not auth_code:
+                logging.info("Authorization code not found, cannot proceed to Step 8.")
+                return
+            
+            json_data = {
+                'client_id': CLIENTID,
+                'code': auth_code,
+                'grant_type': 'authorization_code',
+                'redirect_uri': 'https://www.seek.com.au/oauth/callback/',
+            }
+
+            response = session.post('https://login.seek.com/oauth/token', json=json_data)
+            data = response.json()
+            logging.info(data)
+            bearer = data['access_token']
+
+            # TODO: save tokens
+
+    def _parse_auth_code(self, url):
+        if "code=" in url:            
+            parsed_url = urlparse(url)
+            params = parse_qs(parsed_url.query)
+            
+            auth_code = params.get('code', [None])[0]
+            return auth_code
+        return
